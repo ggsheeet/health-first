aa2f428037d4520b2055f17499f8c0555b38b90b
Luis Gzz <ggcryptodoc@gmail.com>
2024-05-05 17:48:50 -0600

    stopping touch event propagation in BannerGrid component

--- src/components/bannerGrid/index.tsx
+++ src/components/bannerGrid/index.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+'use client'
+import React, { useEffect} from 'react'
 import { BannerBlog } from './bannerBlog'
 import { BannerCarousel } from '../banner/bannerContainer/bannerCarousel'
 
 const BannerGrid = () => {
+	useEffect(() => {
+		const handleBodyTouchMove = (e: TouchEvent) => {
+			e.preventDefault();
+		};
+	
+		document.body.addEventListener('touchmove', handleBodyTouchMove, {
+			passive: false,
+		});
+	
+		return () => {
+			document.body.removeEventListener('touchmove', handleBodyTouchMove);
+		};
+	}, []);
 	return (
 		<>
 			<BannerCarousel>
